79b738d6f4f4abd6640e7925a236b5f2dafa0e97
jamesparkes <parkesrjames@gmail.com>
2018-10-02 10:27:39 -0500

    diff spot

--- lib/fastlane/plugin/ionic/actions/ionic_action.rb
+++ lib/fastlane/plugin/ionic/actions/ionic_action.rb
@@ -92,7 +92,7 @@ module Fastlane
 
         if params[:cordova_prepare]
           # TODO: Remove params not allowed/used for `prepare`
-          sh "node --max-old-space-size=8192 $(which ionic) cordova prepare #{params[:platform]} #{args.join(' ')}"
+          sh "node --max-old-space-size=8192 $(which ionic) cordova prepare #{params[:platform]} #{args.join(' ')} --buildConfig=build.json"
         end
 
         # special handling for `build_number` param
@@ -108,7 +108,7 @@ module Fastlane
         end
 
         if params[:platform].to_s == 'ios'
-          sh "node --max-old-space-size=8192 $(which ionic) cordova compile #{params[:platform]} #{args.join(' ')} -- #{ios_args} --buildConfig=build.json" 
+          sh "node --max-old-space-size=8192 $(which ionic) cordova compile #{params[:platform]} #{args.join(' ')} -- #{ios_args}" 
         elsif params[:platform].to_s == 'android'
           sh "node --max-old-space-size=8192 $(which ionic) cordova compile #{params[:platform]} #{args.join(' ')} -- -- #{android_args}" 
         end
